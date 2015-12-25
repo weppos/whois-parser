d54f8581feedfd40670d63c27ae915e3357e92ea
Simone Carletti <weppos@weppos.net>
2015-12-25 17:23:49 +0100

    Backport .INFO, .PRO, .IN, .AM, CK changes from whois

---
+++ lib/whois/parsers/whois.afilias.net.rb
@@ -0,0 +1,26 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2015 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_afilias2'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.afilias.net server.
+    class WhoisAfiliasNet < BaseAfilias2
+
+      self.scanner = Scanners::BaseAfilias, {
+          pattern_disclaimer: /^Access to/
+      }
+
+    end
+
+  end
+end
---
+++ lib/whois/parsers/whois.inregistry.net.rb
@@ -0,0 +1,27 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2015 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_afilias'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.inregistry.net server.
+    class WhoisInregistryNet < BaseAfilias
+
+      self.scanner = Scanners::BaseAfilias, {
+          # Disclaimer starts with "Access to" in .in servers
+          pattern_disclaimer: /^Access to/
+      }
+
+    end
+
+  end
+end
