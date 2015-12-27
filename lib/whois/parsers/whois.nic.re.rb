c44033137c7afb114fbcc97cc19af83f146f2c81
Simone Carletti <weppos@weppos.net>
2015-12-27 23:05:17 +0100

    Extracted whois.nic.re from whois.nic.fr
    See https://github.com/weppos/whois/issues/497

---
+++ lib/whois/parsers/whois.nic.re.rb
@@ -0,0 +1,25 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2015 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'base_nic_fr'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.nic.pm server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisNicRe < BaseNicFr
+    end
+
+  end
+end
