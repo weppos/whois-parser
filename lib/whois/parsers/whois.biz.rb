84a3eddd25517d1e9b85fbd24cdc8592b096edb8
John Thomas <thomas07@vt.edu>
2018-02-15 09:44:41 -0500

    whois.biz server moved to icann format

--- lib/whois/parsers/whois.biz.rb
+++ lib/whois/parsers/whois.biz.rb
@@ -7,7 +7,7 @@
 #++
 
 
-require_relative 'base_shared2'
+require_relative 'base_icann_compliant'
 
 
 module Whois
@@ -18,7 +18,16 @@ module Whois
     # @see Whois::Parsers::Example
     #   The Example parser for the list of all available methods.
     #
-    class WhoisBiz < BaseShared2
+    class WhoisBiz < BaseIcannCompliant
+      self.scanner = Scanners::BaseIcannCompliant, {
+          pattern_available: /^No Data Found\n/
+      }
+
+      property_supported :expires_on do
+        node("Registry Expiry Date") do |value|
+          parse_time(value)
+        end
+      end
     end
 
   end
