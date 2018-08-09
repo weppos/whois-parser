8705982513adeefc227bb1cb2651b0a77790411c
Roshan Choxi <roshan.choxi@gmail.com>
2018-08-08 19:00:30 -0500

    Update whois.nic.io fixtures and parser

--- lib/whois/parsers/whois.nic.io.rb
+++ lib/whois/parsers/whois.nic.io.rb
@@ -19,30 +19,40 @@ module Whois
     #   The Example parser for the list of all available methods.
     #
     class WhoisNicIo < BaseIcb
-
       property_supported :domain do
-        if reserved?
-          nil
-        else
-          super()
+        if content_for_scanner =~ /Domain Name:\s+(.*)\n/
+          $1.downcase
         end
       end
 
       property_supported :status do
-        if reserved?
-          :reserved
+        if content_for_scanner.match(/NOT FOUND/)
+          :available
         else
-          super()
+          :registered
         end
       end
 
+      property_supported :available? do
+        status == :available
+      end
 
-      # NEWPROPERTY
-      def reserved?
-        !!content_for_scanner.match(/^Domain reserved\n/)
+      property_supported :registered? do
+        status == :registered
+      end
+
+      property_supported :expires_on do
+        if content_for_scanner =~ /Registry Expiry Date:\s+(.*)\n/
+          parse_time($1)
+        end
+      end
+
+      property_supported :nameservers do
+        content_for_scanner.scan(/Name Server:\s+(.*)/).map do |name|
+          Parser::Nameserver.new(:name => name[0].strip)
+        end
       end
 
     end
-
   end
 end
