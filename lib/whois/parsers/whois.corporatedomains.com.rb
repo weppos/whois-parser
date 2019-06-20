309f3ecd1ac029dc4af17071fe56feb1322a98b4
Nicole Ng <nng@itglue.com>
2019-06-20 13:56:18 -0700

    Fixed the WHOIS parser for corporatedomains dates using '/'
    Updated the WHOIS parser for corporatedomains which were incorrectly
    parsing "Registrar Registration Expiration Date", because the date had
    "MM/DD/YYYY" format rather than "DD/MM/YYYY". The change updates
    the format if the value has slash-separated date before passing it into
    `parse_time`. This change cannot be used for all dates, since some dates
    use a dash-separated format (i.e. YYYY-MM-DDTHH:MM:SSZ).
    
    This problem also applies to the creation date as well when parsing the
    value for "Creation Date".

--- lib/whois/parsers/whois.corporatedomains.com.rb
+++ lib/whois/parsers/whois.corporatedomains.com.rb
@@ -13,7 +13,7 @@ require_relative 'base_icann_compliant'
 module Whois
   class Parsers
 
-    # Parser for the whois.markmonitor.com server.
+    # Parser for the whois.corporatedomains.com server.
     #
     # @see Whois::Parsers::Example
     #   The Example parser for the list of all available methods.
@@ -22,6 +22,39 @@ module Whois
       self.scanner = Scanners::BaseIcannCompliant, {
           pattern_available: /^No match for/
       }
+
+      property_supported :created_on do
+        node("Creation Date") do |value|
+          # Update the format so the value is parsed properly
+          value = reformat_slash_date_format(value) if value.include?("/")
+
+          parse_time(value)
+        end
+      end
+
+      property_supported :expires_on do
+        node("Registrar Registration Expiration Date") do |value|
+          # Update the format so the value is parsed properly
+          value = reformat_slash_date_format(value) if value.include?("/")
+
+          parse_time(value)
+        end
+      end
+
+      private
+
+      # There are 2 different date formats which are used for creation and expiration dates:
+      #   - hyphen-separated date format YYYY-MM-DDTHH:MM:SSZ
+      #   - slash-separated date format MM/DD/YYYY HH:MM:SS
+      #
+      # The latter format causes problems because `parse_time` parsed as (DD/MM/YYYY HH:MM:SS)
+      def reformat_slash_date_format(value)
+        date, time = value.split
+        month, day, year = date.split("/")
+        reformatted_date = [year, month, day].join("-")
+
+        "#{reformatted_date}T#{time}Z"
+      end
     end
 
   end
