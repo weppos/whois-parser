e13c758315d9bc54ae77d56aa517c9d18bf6188d
Raphaël Beamonte <raphael.beamonte@shopify.com>
2019-11-07 15:22:12 -0500

    parsers: whois.cira.ca: Add new version support for 2019 whois output
    Most recent whois output from CIRA, with year 2019, has a very different
    output than what was expected in the past. This adds support for it
    while keeping support for previous versions of the whois output.

--- lib/whois/parsers/whois.cira.ca.rb
+++ lib/whois/parsers/whois.cira.ca.rb
@@ -31,6 +31,7 @@ module Whois
 
 
       property_supported :domain do
+        return node("Domain Name") || node("Not found") if version == "3"
         node("Domain name")
       end
 
@@ -38,6 +39,129 @@ module Whois
 
 
       property_supported :status do
+        return status_v3 if version == "3"
+        status_v1
+      end
+
+      property_supported :available? do
+        status == :available
+      end
+
+      property_supported :registered? do
+        !available?
+      end
+
+
+      property_supported :created_on do
+        node_name = if version == "3"
+          "Creation Date"
+        else
+          "Creation date"
+        end
+
+        node(node_name) { |str| parse_time(str) }
+      end
+
+      property_supported :updated_on do
+        node_name = if version == "3"
+          "Updated Date"
+        else
+          "Updated date"
+        end
+
+        node(node_name) { |str| parse_time(str) }
+      end
+
+      property_supported :expires_on do
+        node_name = if version == "3"
+          "Registry Expiry Date"
+        else
+          "Expiry date"
+        end
+
+        node(node_name) { |str| parse_time(str) }
+      end
+
+
+      property_supported :registrar do
+        return registrar_v3 if version == "3"
+        registrar_v1
+      end
+
+
+      property_supported :registrant_contacts do
+        build_contact(Parser::Contact::TYPE_REGISTRANT)
+      end
+
+      property_supported :admin_contacts do
+        build_contact(Parser::Contact::TYPE_ADMINISTRATIVE)
+      end
+
+      property_supported :technical_contacts do
+        build_contact(Parser::Contact::TYPE_TECHNICAL)
+      end
+
+      # Nameservers are listed in the following formats:
+      #
+      #   ns1.google.com
+      #   ns2.google.com
+      #
+      #   ns1.google.com  216.239.32.10
+      #   ns2.google.com  216.239.34.10
+      #
+      property_supported :nameservers do
+        node_name = if version == "3"
+          "Name Server"
+        else
+          "field:nameservers"
+        end
+
+        Array.wrap(node(node_name)).map do |line|
+          name, ipv4 = line.strip.split(/\s+/)
+          Parser::Nameserver.new(:name => name, :ipv4 => ipv4)
+        end
+      end
+
+      # Attempts to detect and returns the version.
+      #
+      # TODO: This is very empiric.
+      #       Use the available status in combination with the creation date label.
+      #
+      # NEWPROPERTY
+      def version
+        cached_properties_fetch :version do
+          version = if content_for_scanner =~ /^% \(c\) (.+?) Canadian Internet Registration Authority/
+            year = $1.to_i
+            if year >= 2019
+              "3"
+            elsif year >= 2010
+              "2"
+            elsif year >= 2007
+              "1"
+            end
+          end
+          version || Whois::Parser.bug!(ParserError, "Unable to detect version.")
+        end
+      end
+
+      # NEWPROPERTY
+      def valid?
+        cached_properties_fetch(:valid?) do
+          !invalid?
+        end
+      end
+
+      # NEWPROPERTY
+      def invalid?
+        cached_properties_fetch(:invalid?) do
+          status == :invalid
+        end
+      end
+
+
+      private
+
+      def status_v1
         if content_for_scanner =~ /Domain status:\s+(.+?)\n/
           case node("Domain status", &:downcase)
           when "registered"
@@ -62,29 +186,15 @@ module Whois
         end
       end
 
-      property_supported :available? do
-        status == :available
+      def status_v3
+        if node("Not found")
+          :available
+        else
+          :registered
+        end
       end
 
-      property_supported :registered? do
-        !available?
-      end
-
-
-      property_supported :created_on do
-        node("Creation date") { |str| parse_time(str) }
-      end
-
-      property_supported :updated_on do
-        node("Updated date") { |str| parse_time(str) }
-      end
-
-      property_supported :expires_on do
-        node("Expiry date") { |str| parse_time(str) }
-      end
-
-
-      property_supported :registrar do
+      def registrar_v1
         node("Registrar") do |hash|
           Parser::Registrar.new(
             id:           hash["Number"],
@@ -94,80 +204,37 @@ module Whois
         end
       end
 
+      def registrar_v3
+        registrar_information = {
+          id:           node("Registrar IANA ID"),
+          name:         node("Registrar"),
+          organization: node("Registrar"),
+        }
+        registrar_information.compact!
+        return if registrar_information.empty?
 
-      property_supported :registrant_contacts do
-        build_contact("Registrant", Parser::Contact::TYPE_REGISTRANT)
+        Parser::Registrar.new(
+          **registrar_information,
+        )
       end
 
-      property_supported :admin_contacts do
-        build_contact("Administrative contact", Parser::Contact::TYPE_ADMINISTRATIVE)
+      def build_contact(type)
+        return build_contact_v3(type) if version == "3"
+        build_contact_v1(type)
       end
 
-      property_supported :technical_contacts do
-        build_contact("Technical contact", Parser::Contact::TYPE_TECHNICAL)
-      end
-
-
-      property_supported :nameservers do
-        Array.wrap(node("nserver")).map do |line|
-          name, ipv4 = line.split(/\s+/)
-          Parser::Nameserver.new(:name => name, :ipv4 => ipv4)
+      def build_contact_v1(type)
+        element = case type
+        when Parser::Contact::TYPE_REGISTRANT
+          'Registrant'
+        when Parser::Contact::TYPE_ADMINISTRATIVE
+          'Administrative contact'
+        when Parser::Contact::TYPE_TECHNICAL
+          'Technical contact'
+        else
+          Whois::Parser.bug!(ParserError, "Invalid contact type #{type}.")
         end
-      end
 
-
-      # Nameservers are listed in the following formats:
-      #
-      #   ns1.google.com
-      #   ns2.google.com
-      #
-      #   ns1.google.com  216.239.32.10
-      #   ns2.google.com  216.239.34.10
-      #
-      property_supported :nameservers do
-        Array.wrap(node("field:nameservers")).map do |line|
-          name, ipv4 = line.strip.split(/\s+/)
-          Parser::Nameserver.new(:name => name, :ipv4 => ipv4)
-        end
-      end
-
-
-      # Attempts to detect and returns the version.
-      #
-      # TODO: This is very empiric.
-      #       Use the available status in combination with the creation date label.
-      #
-      # NEWPROPERTY
-      def version
-        cached_properties_fetch :version do
-          version = if content_for_scanner =~ /^% \(c\) (.+?) Canadian Internet Registration Authority/
-            case $1
-            when "2007" then "1"
-            when "2010" then "2"
-            end
-          end
-          version || Whois::Parser.bug!(ParserError, "Unable to detect version.")
-        end
-      end
-
-      # NEWPROPERTY
-      def valid?
-        cached_properties_fetch(:valid?) do
-          !invalid?
-        end
-      end
-
-      # NEWPROPERTY
-      def invalid?
-        cached_properties_fetch(:invalid?) do
-          status == :invalid
-        end
-      end
-
-
-      private
-
-      def build_contact(element, type)
         node(element) do |hash|
           Parser::Contact.new(
             :type         => type,
@@ -184,6 +251,41 @@ module Whois
             :email        => hash["Email"]
           )
         end
+      end
+
+      def build_contact_v3(type)
+        prefix = case type
+        when Parser::Contact::TYPE_REGISTRANT
+          'Registrant'
+        when Parser::Contact::TYPE_ADMINISTRATIVE
+          'Admin'
+        when Parser::Contact::TYPE_TECHNICAL
+          'Tech'
+        else
+          Whois::Parser.bug!(ParserError, "Invalid contact type #{type}.")
+        end
+
+        contact_information = {
+          id:           node("Registry #{prefix} ID"),
+          name:         node("#{prefix} Name"),
+          organization: node("#{prefix} Organization"),
+          address:      node("#{prefix} Street"),
+          city:         node("#{prefix} City"),
+          zip:          node("#{prefix} Postal Code"),
+          state:        node("#{prefix} State/Province"),
+          country:      node("#{prefix} Country"),
+          country_code: node("#{prefix} Country"),
+          phone:        node("#{prefix} Phone"),
+          fax:          node("#{prefix} Fax"),
+          email:        node("#{prefix} Email"),
+        }
+        contact_information.compact!
+        return if contact_information.empty?
+
+        Parser::Contact.new(
+          type: type,
+          **contact_information,
+        )
       end
 
     end
