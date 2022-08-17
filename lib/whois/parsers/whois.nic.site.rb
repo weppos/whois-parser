fecafd7b136562a0a292b43e9c29e67e0015c4e8
Felipe Endo <felipe.endo@rebase.com.br>
2022-08-17 04:43:55 -0300

    Add parser for whois.nic.site

---
+++ lib/whois/parsers/whois.nic.site.rb
@@ -0,0 +1,28 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2018 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require_relative 'whois.centralnic.com'
+
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.nic.site server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisNicSite < WhoisCentralnicCom
+    end
+
+  end
+end
+
+
+
